4ca0fb0b47aa28319600931fe9ff47b687cfcad9
Danilo Horta <danilo.horta@gmail.com>
2016-12-28 13:14:18 +0000

    install armadillo

---
+++ travis/install_armadillo.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+wget https://sourceforge.net/projects/arma/files/armadillo-7.600.2.tar.xz
+tar xvfJ armadillo-7.600.2.tar.xz
+
+pushd armadillo-7.600.2
+perl -i -pe 's/ARMA_USE_WRAPPER true/ARMA_USE_WRAPPER false/gc' CMakeLists.txt
+perl -i -pe 's/armadillo PROPERTIES/armadillo PROPERTIES POSITION_INDEPENDENT_CODE ON/gc' CMakeLists.txt
+
+mkdir build && cd build
+cmake .. && make && sudo make install
+popd
