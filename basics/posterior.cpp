7699107fb0e877ef95095c2b7d9ede5fdb26ea73
Danilo Horta <danilo.horta@gmail.com>
2016-12-28 16:27:35 +0000

    remove auto, uvec

--- basics/posterior.cpp
+++ basics/posterior.cpp
@@ -4,6 +4,7 @@ using arma::log;
 using arma::accu;
 using arma::sum;
 using arma::find;
+using arma::uvec;
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -13,10 +14,10 @@ double kappaj_post_loglik(const vec &xj, const vec &mu, const vec &delta,
                           const Phij &phij, double nuj, double sj, double theta,
                           double kappa_var) {
   cout << "ponto 1" << endl << flush;
-  auto pos = find(delta > 0);
+  uvec pos = find(delta > 0);
 
   cout << "ponto 2" << endl << flush;
-  auto id = 1 / delta(pos);
+  vec id = 1 / delta(pos);
 
   cout << "ponto 3" << endl << flush;
   double phij_ = phij.get();
@@ -30,14 +31,6 @@ double kappaj_post_loglik(const vec &xj, const vec &mu, const vec &delta,
   double a = sum(xj(pos)) * log(phij_);
   cout << "a" << a << endl << flush;
 
-  cout << "id" << id << endl << flush;
-  cout << "(xj(pos) + id)" << (xj(pos) + id) << endl << flush;
-  cout << "mu(pos) * phij_ * nuj + id" << mu(pos) * phij_ * nuj + id << endl << flush;
-  cout << "log(mu(pos) * phij_ * nuj + id)" << log(mu(pos) * phij_ * nuj + id) << endl << flush;
-  cout << "antes da sum:" << endl << flush;
-  cout << (xj(pos) + id) % log(mu(pos) * phij_ * nuj + id) << endl << flush;
-  cout << "depois da sum:" << endl << flush;
-  cout << sum((xj(pos) + id) % log(mu(pos) * phij_ * nuj + id)) << endl << flush;
   cout << "separacao" << endl << flush;
   cout << -sum((xj(pos) + id) % log(mu(pos) * phij_ * nuj + id)) << endl << flush;
   cout << "-------------" << endl << flush;
