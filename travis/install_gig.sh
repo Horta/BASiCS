ffcfbd76630853c8fcef6f616cf8874b824d1870
Danilo Horta <danilo.horta@gmail.com>
2017-01-02 23:15:12 -0200

    install gig

---
+++ travis/install_gig.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+wget https://github.com/Horta/gig/archive/v0.0.3.tar.gz
+tar xvfJ v0.0.3.tar.gz
+
+pushd gig-0.0.3
+mkdir build && cd build
+cmake .. && make && make test && sudo make install
+popd
